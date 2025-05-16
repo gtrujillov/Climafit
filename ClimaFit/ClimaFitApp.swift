d7298dc290b7ac74a546f00fd0f3dad959cf06da
Gonzalo Trujillo Vallejo <gonzalo.trujillo@babelgroup.com>
2025-05-16 14:29:48 +0200

    Primer commit de ClimaFit

---
+++ ClimaFit/ClimaFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaFitApp.swift
+//  ClimaFit
+//
+//  Created by Gonzalo Trujillo Vallejo on 16/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ClimaFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimaFit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClimaFit
+//
+//  Created by Gonzalo Trujillo Vallejo on 16/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
